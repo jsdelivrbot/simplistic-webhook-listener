6524b7237aefaf9d2c38cc313b9664090f87b3ca
Benedikt von St. Vieth <b.von.st.vieth@fz-juelich.de>
2016-07-04 11:21:55 +0200

    improve token compare functions if token list is used

--- hooker/__init__.py
+++ hooker/__init__.py
@@ -7,18 +7,22 @@ def compare(tokens, auth_header):
     major = sys.version_info.major
     minor = sys.version_info.minor
     micro = sys.version_info.micro
+
+    logging.debug(tokens)
     for token in tokens.split(';'):
         if (major == 2 and minor <= 7 and micro < 7) \
                 or (major == 3 and minor < 3):
             logging.debug('Using own compare function for authorization token')
             valid = True
             if len(token) != len(auth_header):
-                return False
+                valid = False
             for iterator, item in enumerate(token):
                 if item != auth_header[iterator]:
                     valid = False
-            return valid
+            if valid:
+                return True
         else:
             logging.debug('Using hmac compare function for auth token')
             import hmac
-            return hmac.compare_digest(unicode(token), auth_header)
+            if hmac.compare_digest(unicode(token), auth_header):
+                return True
