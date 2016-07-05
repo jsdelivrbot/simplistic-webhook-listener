8549660da6d3f64f50e5f733731ec38561bd03f5
Benedikt von St. Vieth <b.von.st.vieth@fz-juelich.de>
2016-07-05 14:23:48 +0200

    placeholders for asses_reload via travis/github

--- hooker/github.py
+++ hooker/github.py
@@ -3,3 +3,6 @@
 def authenticate(tokens, request):
     # TODO: check headers/payload and implement it for github
     return True
+
+def assess_reload(request):
+    return True
--- hooker/travis.py
+++ hooker/travis.py
@@ -5,3 +5,18 @@ def authenticate(tokens, request):
     auth_header = request.headers.get('Authorization')
 
     return compare(tokens, auth_header) if auth_header else False
+
+def assess_reload(request):
+    webhook_action = request.headers.get('X-Gitlab-Event')
+    if webhook_action == u'Build Hook':
+        payload = request.get_json()
+        if payload and payload['build_status'] == 'success':
+            logging.debug('Got successfull build_status from gitlab, reload')
+            return True
+        else:
+            return False
+    elif webhook_action == u'Push Hook':
+        logging.debug('Got push from gitlab, reload')
+        return True
+    else:
+        return False
